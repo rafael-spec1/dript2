4f30009c5eeccb3d745e926ddeac25e59d02b430
rafael-spec1 <rafaelinacio7100@gmail.com>
2026-07-11 23:12:50 -0300

    pagina de produto, e design no css/ comeco da organizacao pesada no js para botoes funcionais

--- js/script.js
+++ js/script.js
@@ -152,21 +152,6 @@ window.onload = function () {
         }
 }
 
-function favoritar(botao) {
-        botao.classList.toggle("favoritado");
-        let favoritos = localStorage.getItem("favoritos");
-        if (favoritos === null) {
-                favoritos = 0;
-        }
-        favoritos = Number(favoritos);
-        if (botao.classList.contains("favoritado")) {
-                favoritos++;
-        } else {
-                favoritos--;
-        }
-        localStorage.setItem("favoritos", favoritos);
-        document.getElementById("contador-favoritos").textContent = favoritos;
-}
 
 function filtrarProdutos(categoria) {
         let produtos = document.querySelectorAll(".produto");
@@ -212,3 +197,60 @@ window.addEventListener("load", () => {
                 }, 1000);
         }, 2000);
 });
+
+function favoritarProduto(botao) {
+        const produto = {
+                nome: "Moletom DRIPT",
+                preco: "R$ 179,90",
+                imagem: "../img/moletom-rio-arabe-black.png"
+        };
+        let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+        const existe = favoritos.find(item => item.nome === produto.nome);
+        if (existe) {
+                favoritos = favoritos.filter(item => item.nome !== produto.nome);
+                botao.textContent = "🤍";
+                botao.classList.remove("ativo");
+        } else {
+                favoritos.push(produto);
+                botao.textContent = "❤️";
+                botao.classList.add("ativo");
+        }
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+        atualizarContadorFavoritos();
+        carregarFavoritos();
+}
+
+function carregarFavoritos() {
+        const lista = document.getElementById("lista-favoritos");
+        if (!lista) return;
+        const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+        lista.innerHTML = "";
+        if (favoritos.length === 0) {
+                lista.innerHTML = "<p>Nenhum produto favoritado.</p>";
+                return;
+        }
+        favoritos.forEach(produto => {
+                lista.innerHTML += `
+                <div class="produto">
+                <img src="${produto.imagem}" alt="${produto.nome}">
+                <h3>${produto.nome}</h3>
+                <p>${produto.preco}</p>
+                </div>
+        `;
+        });
+}
+
+
+function atualizarContadorFavoritos() {
+
+        const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+        const contador = document.getElementById("contador-favoritos");
+
+        if (contador) {
+                contador.textContent = favoritos.length;
+        }
+}
+
+carregarFavoritos();
+atualizarContadorFavoritos();
